e05da9327af123296d48af3e05ef6fb071684895
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-07-21 15:21:45 +0900

    feat: llm-openrouter

---
+++ pkgs/tree/llm-openrouter/default.nix
@@ -0,0 +1,20 @@
+# Expects mcp > 1.6.0. See overlays/python-packages/mcp
+{
+  pkgs,
+  lib,
+  pyproject-nix,
+  ...
+}:
+let 
+  python = pkgs.python3;
+  project = pyproject-nix.lib.project.loadPyproject {
+    projectRoot = pkgs.fetchFromGitHub {
+      owner = "simonw";
+      repo = "llm-openrouter";
+      tag = "0.4.1";
+      hash = "sha256-ojBkyXqEaqTcOv7mzTWL5Ihhb50zeVzeQZNA6DySuVg=";
+    };
+  };
+  attrs = project.renderers.buildPythonPackage { inherit python; };
+in 
+pkgs.python3Packages.buildPythonPackage attrs
