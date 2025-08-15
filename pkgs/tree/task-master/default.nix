d08b382d9d08ec338fe4412a354150bc5e4a8c56
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-08-15 20:35:46 +0900

    refactor(pkgs): rename mcp-server-task-master to task-master and update build method

--- pkgs/tree/task-master/default.nix
+++ pkgs/tree/task-master/default.nix
@@ -4,8 +4,8 @@
   lib,
   ...
 }:
-pkgs.stdenv.mkDerivation rec {
-  pname = "mcp-server-task-master";
+pkgs.buildNpmPackage rec {
+  pname = "task-master";
   version = "0.18.0";
   src = pkgs.fetchFromGitHub {
     owner = "eyaltoledano";
@@ -14,33 +14,21 @@ pkgs.stdenv.mkDerivation rec {
     hash = "sha256-RnbquGcanpBH5A++MZOVNLXEdn7qVJIVWxUOZEBpF/o=";
   };
 
+  buildInputs = with pkgs; [ nodejs ];
+
   npmDeps = pkgs.importNpmLock {
     npmRoot = src;
   };
 
   npmConfigHook = pkgs.importNpmLock.npmConfigHook;
 
-  installPhase = ''
-    runHook preInstall
-
-    ls -al
-    mkdir -p $out/bin
-    for file in *; do
-      cp -r $file $out/
-    done
-    echo '#!${pkgs.bash}/bin/bash' > $out/bin/task-master-ai
-    echo "export NODE_PATH=$out:$out/src; ${pkgs.nodejs}/bin/node $out/mcp-server/server.js \"\$@\"" >> $out/bin/task-master-ai
-    chmod +x $out/bin/task-master-ai
-    
-    runHook postInstall
-  '';
-
+  dontBuild = true;
   dontCheckForBrokenSymlinks = true;
 
   meta = {
     description = "An AI-powered task-management system you can drop into Cursor, Lovable, Windsurf, Roo, and others.";
     homepage = "https://github.com/eyaltoledano/claude-task-master";
     license = lib.licenses.mit;
-    mainProgram = "task-master-ai";
+    mainProgram = "task-master";
   };
 }
