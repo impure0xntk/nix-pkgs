7ca5be64dd9caba525203b1ba72935221c72129a
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-10-04 21:10:20 +0900

    feat(mcpjungle): bump to 0.2.14

--- pkgs/tree/mcpjungle/default.nix
+++ pkgs/tree/mcpjungle/default.nix
@@ -6,14 +6,14 @@
 }:
 pkgs.buildGoModule rec {
   pname = "mcpjungle";
-  version = "0.2.8";
+  version = "0.2.14";
   src = pkgs.fetchFromGitHub {
     owner = "mcpjungle";
     repo = "MCPJungle";
     tag = version;
-    hash = "sha256-DFWLqkjX8YLE9veLrPyxW3wkjJqDrtoOvnU1+UrqPX4=";
+    hash = "sha256-rdvn9rRZrXSRShAY0V10MvC2Bj1i1If5MoSTaRYZvNg=";
   };
-  vendorHash = "sha256-XpYG11WeX0AX6FDcl4YjveDaZ3vwK2oUNDutMRTqIUc=";
+  vendorHash = "sha256-szv2P9qFPALSBSjlmMJTAXcXDFn/wBuSqVlxYtoXX+s=";
 
   meta = {
     description = "Self-hosted MCP Gateway and Registry for AI agents";
