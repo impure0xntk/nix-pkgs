5a9ea9b56725644ed37cf890ba9d6c0df61f85f9
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2025-10-18 17:55:31 +0900

    chore(mcp-server-devtools): bump version

--- pkgs/tree/mcp-server-devtools/default.nix
+++ pkgs/tree/mcp-server-devtools/default.nix
@@ -6,15 +6,15 @@
 }:
 pkgs.buildGo125Module (self: rec { # TODO: remove buildGo"125"Module after NixOS 25.11 to use go latest
   pname = "mcp-server-devtools";
-  version = "0.40.7";
+  version = "0.40.10";
 
   src = pkgs.fetchFromGitHub {
     owner = "sammcj";
     repo = "mcp-devtools";
     tag = "v${version}";
-    hash = "sha256-HR1Nh1MFRRIGH2D4TAlvhMH9CN6FOErUL9MvcI1kXbM=";
+    hash = "sha256-Zo7aJYeHL+wbsjZBRrVE4wTk8IXN/ZcpY5+pHYC11M4=";
   };
-  vendorHash = "sha256-F6/U7cU9L3+I5ragr6BdG5z/nomVwiQZujNs13Y3oo4=";
+  vendorHash = "sha256-I9hC0mll47oz50Jwd3c0hyxdbq2IBNgXwh4Db7nsq2o=";
 
   doCheck = false; # some deps try to connect internet
 
