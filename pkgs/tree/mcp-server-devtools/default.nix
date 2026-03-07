26ce1913cf0b89e94cc16ef644b886d9e6f096fa
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2026-03-08 02:24:24 +0900

    refactor(mcp): devtools: update to version 0.59.31 and switch to buildGo126Module, and remove unused markitdown and playwright packages

--- pkgs/tree/mcp-server-devtools/default.nix
+++ pkgs/tree/mcp-server-devtools/default.nix
@@ -4,29 +4,39 @@
   lib,
   ...
 }:
-pkgs.pure-unstable.buildGoModule (self: rec {
-  pname = "mcp-server-devtools";
-  version = "0.59.3";
+let
+  package = pkgs.pure-unstable.buildGo126Module (self: rec {
+    pname = "mcp-server-devtools";
+    version = "0.59.31";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "sammcj";
-    repo = "mcp-devtools";
-    tag = "v${version}";
-    hash = "sha256-5Ljns7ZiypFm2CMjQz6MHw731JE3EURNSPfdjfoeXEs=";
-  };
-  vendorHash = "sha256-hPpu8a0ExdZk33xRFZUp+Vc4xPpNpO4UPOW1wqDZsHY=";
+    src = pkgs.fetchFromGitHub {
+      owner = "sammcj";
+      repo = "mcp-devtools";
+      tag = "v${version}";
+      hash = "sha256-zd+RBiZP1QnmH3XWzMpNta9RV1apy7V+7MvNUokm28s=";
+    };
+    vendorHash = "sha256-fvCY57PsUZw07zbqxHbBj60tYtULC1AtJDzYDmAQ/wA=";
 
-  # tests/benchmarks build failure workaround
-  preBuild = ''
-    rm -rf tests
-  '';
+    # tests/benchmarks build failure workaround
+    preBuild = ''
+      rm -rf tests
+    '';
 
-  doCheck = false; # some deps try to connect internet
+    doCheck = false; # some deps try to connect internet
 
-  meta = {
-    description = "A modular MCP server that provides commonly used developer tools for AI coding agents";
-    homepage = "https://github.com/sammcj/mcp-devtools";
-    license = lib.licenses.asl20;
-    mainProgram = "mcp-devtools";
+    meta = {
+      description = "A modular MCP server that provides commonly used developer tools for AI coding agents";
+      homepage = "https://github.com/sammcj/mcp-devtools";
+      license = lib.licenses.asl20;
+      mainProgram = "mcp-devtools";
+    };
+  });
+in package.overrideAttrs (old: {
+  passthru = old.passthru // {
+    withPackages = ps:
+      pkgs.runCommand "mcp-server-devtools-with-deps" { nativeBuildInputs = [ pkgs.makeWrapper ]; } ''
+        makeWrapper ${package}/bin/${package.meta.mainProgram} $out/bin/${package.meta.mainProgram} \
+          --set PATH ${lib.makeBinPath ps}
+    '';
   };
 })
