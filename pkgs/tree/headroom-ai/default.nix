ea15a9147cf8ded7dd9389421716431cd2256382
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2026-04-29 12:12:28 +0900

    feat: bump headroom-ai

--- pkgs/tree/headroom-ai/default.nix
+++ pkgs/tree/headroom-ai/default.nix
@@ -4,13 +4,13 @@ let
 in
 python.python3Packages.buildPythonPackage rec {
   pname = "headroom-ai";
-  version = "0.5.20";
+  version = "0.14.1";
   pyproject = true;
 
   src = python.fetchPypi {
     pname = "headroom_ai";
     inherit version;
-    hash = "sha256-vJf8eSjeGynxDlwWNOJvDrnhAJQjT9nPrNXPPJMQ7Wg=";
+    hash = "sha256-UL2ATzDr2WuRUALgqzaGIrkV9xxBI2OYSHkzd3kmaLw=";
   };
 
   nativeBuildInputs = with python.python3Packages; [
@@ -23,6 +23,9 @@ python.python3Packages.buildPythonPackage rec {
     litellm
     click
     rich
+    opentelemetry-api
+    # ast-grep-cli -> ast-grep binary
+    tomli
 
     # proxy
     fastapi
@@ -36,10 +39,19 @@ python.python3Packages.buildPythonPackage rec {
     websockets
     onnxruntime
     transformers
+    watchdog
 
     # code
     tree-sitter-language-pack
+  ] ++ [
+    pkgs.pure-unstable.ast-grep # instead of ast-grep-cli
   ];
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail '"ast-grep-cli>=0.30.0",' "" \
+      --replace-fail '"litellm==1.82.3",' '"litellm>=1.82.3",'
+  '';
 
   doCheck = false;
 
