ba48d0ef111f9bab567a06f7d251d13c99b6d9ca
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2026-05-07 08:58:26 +0900

    feat: switch mvnd to unstable

--- pkgs/tree/mvnd/default.nix
+++ pkgs/tree/mvnd/default.nix
@@ -1,7 +1,7 @@
 # This includes maven, and specific library for mavend.
 # TODO: remove and delefate to pkgs.my.mvnd after fixing https://github.com/NixOS/nixpkgs/pull/407817
 { pkgs, prev, ... }:
-(prev.mvnd.overrideAttrs (new: old: {
+(pkgs.unstable.mvnd.overrideAttrs (new: old: {
   passthru.maven = new.finalPackage.out + "/mvnd-home/mvn";
 })).override {
   maven = pkgs.unstable.maven;
