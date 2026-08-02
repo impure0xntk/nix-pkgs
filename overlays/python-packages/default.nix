7ae118562f2b284816ab0eb86c9c820ba124b9e3
impure0xntk <219413815+impure0xntk@users.noreply.github.com>
2026-08-02 21:42:22 +0900

    fix: python overlay

--- overlays/python-packages/default.nix
+++ overlays/python-packages/default.nix
@@ -4,6 +4,17 @@
   pyproject-build-systems,
 }:
 final: prev:
+let 
+  # Python overlays: https://discourse.nixos.org/t/add-python-package-via-overlay/19783/4
+  pythonPackagesOverlays =  (prev.pythonPackagesOverlays or [ ]) ++ [
+    (pyself: pysuper: let
+      # args = { inherit final prev pyself pysuper; };
+      in {
+        # Not used, just keep for python overlay examples.
+        # mcp = import ./mcp args;
+      })
+  ];
+in
 {
   # Inspire: https://github.com/timblaktu/mcp-servers-nix/blob/main/pkgs/reference/generic-uv2nix-proper.nix
   buildUvPackage =
@@ -36,20 +47,11 @@ final: prev:
     in
     pythonSet.mkVirtualEnv "${pname}-env" workspace.deps.default;
 
-  # Python overlays: https://discourse.nixos.org/t/add-python-package-via-overlay/19783/4
-  pythonPackagesOverlays =  (prev.pythonPackagesOverlays or [ ]) ++ [
-    (pyself: pysuper: let
-      # args = { inherit final prev pyself pysuper; };
-      in {
-        # Not used, just keep for python overlay examples.
-        # mcp = import ./mcp args;
-      })
-  ];
-  python3 = let self = prev.python3.override {
-    inherit self;
-    packageOverrides = prev.lib.composeManyExtensions final.pythonPackagesOverlays;
-  }; in self;
-  python3Packages = final.python3.pkgs;
+  # python3 = let self = prev.python3.override {
+  #   inherit self;
+  #   packageOverrides = prev.lib.composeManyExtensions final.pythonPackagesOverlays;
+  # }; in self;
+  # python3Packages = final.python3.pkgs;
 
   inherit pyproject-nix;
 }
